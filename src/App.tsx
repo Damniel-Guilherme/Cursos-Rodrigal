38d0e9a75ae6ff73d0752872ff041873ef07971f
Damniel-Guilherme <danielguilhermetb@gmail.com>
2026-06-10 16:52:21 -0300

    TEST2

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import { signOut } from 'firebase/auth'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
-import ProtectedRoute from './assets/components/ProtectedRoute'
-import { auth } from './services/firebase'
-
-function Dashboard() {
-  const navigate = useNavigate()
-
-  async function handleLogout() {
-    await signOut(auth)
-    navigate('/')
-  }
-
-  return (
-    <div style={{ color: 'white', padding: 40 }}>
-      <h1>Bem-vindo ao Dashboard</h1>
-
-      <button onClick={handleLogout}>
-        Sair
-      </button>
-    </div>
-  )
-}
+import Dashboard from '../pages/Dashboard'
+import ProtectedRoute from '../src/assets/components/ProtectedRoute'
 
 function App() {
   return (
